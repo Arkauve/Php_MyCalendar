97bc4623f126588038090749a6d549988e089b2c
Romain <garnierr10@gmail.com>
2016-05-25 11:25:43 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
 
 	/**
-	 * connexion a la base de donnée
+	 * connexion a la base de donnÃ©e
 	 * @var unknown
 	 */
-$user="iut_gr1_rg";
-$pass="lama1998";
+$user="user";
+$pass="pass";
 $host="localhost";
 $DB = mysql_connect($host,$user,$pass);
 if(!$DB){
 	die("Connexion impossible : ".mysql_error());
 }
-mysql_select_db("iut_gr1_rg",$DB);
+mysql_select_db("",$DB);
 ?>
